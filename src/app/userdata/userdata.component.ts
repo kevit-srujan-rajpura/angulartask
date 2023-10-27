96e28cd701eb269572d3768cfb215f15d38c17a9
Srujan Rajpura <141118639+kevit-srujan-rajpura@users.noreply.github.com>
2023-10-27 19:58:25 +0530

    Updated userdata.component.ts

--- src/app/userdata/userdata.component.ts
+++ src/app/userdata/userdata.component.ts
@@ -1,8 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserDetailsService } from '../user-details.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+interface Userdata {
+  id:number;
+  name: string;
+  dateOfBirth: Date;
+  email: string;
+  phone: number;
+  education?: {
+    institutename: string;
+    degree: string;
+    percentage: number;
+
+    hobby?: {
+      Reading: boolean;
+      Traveling: boolean;
+      Sports: boolean;
+      Music: boolean;
+      Dancing: boolean;
+      Playing: boolean;
+      Coding: boolean;
+      Cooking: boolean;
+    };
+  };
+  gender?: string;
+  address?: { addedAddress: string }[];
+}
 
 @Component({
   selector: 'app-show-details',
@@ -10,75 +35,68 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./userdata.component.css'],
 })
 export class UserdataComponent implements OnInit {
-  userDetails:
-    | {
-        name: string;
-        dateOfBirth: Date;
-        email: string;
-        phone: number;
-        education?: {
-          institutename: string;
-          degree: string;
-          percentage: number;
+  userDetails: Userdata | undefined;
+  // headers = new Headers({
+  //   'Content-Type': 'application/json',
+  //   Accept: 'application/json',
+  // });
 
-          hobby?: {
-            Reading: boolean;
-            Traveling: boolean;
-            Sports: boolean;
-            Music: boolean;
-            Dancing: boolean;
-            Playing: boolean;
-            Coding: boolean;
-            Cooking: boolean;
-          };
-        };
-        gender?: string;
-        address?: { addedAddress: string }[];
-        summary?: string;
-      }
-    | undefined;
-  displayedColumns: string[] = [
-    'name',
-    'dateOfBirth',
-    'email',
-    'phone',
-    'institutename',
-    'degree',
-    'percentage',
-    'hobby',
-    'gender',
-    'addresses',
-    'summary',
-  ];
-
+  // httpOptions = {
+  //   headers: this.headers,
+  // };
 
   constructor(
     private router: Router,
-   
+    private http: HttpClient,
     private userDetailsService: UserDetailsService
   ) {}
 
   ngOnInit() {
     this.userDetailsService.userDetailsObservable$.subscribe((userDetails) => {
       if (userDetails) {
-        // true when the obj comes
-        this.userDetails = userDetails; // value setted here to show
+        console.log(userDetails.id)
+        this.http
+          .get<Userdata[]>(' http://localhost:3000/signup')
+          .subscribe((res) => {
+            if (res && res.length > 0) {
+              this.userDetails = res[res.length - 1];
+            } else {
+              alert('Ther is no user exists');
+            }
+          });
       } else {
-        this.router.navigate(['/user-details']);
+        this.router.navigate(['/signup']);
       }
     });
   }
 
   editForm() {
-    // this.userDetailsService.setUserDetails(this.userDetails);
-    this.router.navigate(['/user-details']);
+    this.router.navigate(['/updateuser']);
   }
 
-  allusersdata(){
+  allusersdata() {
     this.router.navigate(['/alluserdata']);
   }
 
-  deleteUser(){
-    
+  deleteUser(userDetails: Userdata["id"]) {
+    if (this.userDetails) {
+      const userIdToDelete = this.userDetails['id'];
+      this.http
+        .delete(`http://localhost:3000/signup/${userIdToDelete}`)
+        .subscribe(
+          (response) => {
+            console.log(`User with ID ${userIdToDelete} has been deleted.`);
+            if (this.userDetails) {
+              this.userDetails = undefined;
+            }
+          },
+          (error) => {
+            console.error(
+              `Error deleting user with ID ${userIdToDelete}:`,
+              error
+            );
+          }
+        );
+    }
   }
 }
