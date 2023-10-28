bca384334fb6b5623e7bc5cf06877edba00e0e15
Srujan Rajpura <141118639+kevit-srujan-rajpura@users.noreply.github.com>
2023-10-28 18:23:24 +0530

    Updated updateuser.component.ts

--- src/app/updateuser/updateuser.component.ts
+++ src/app/updateuser/updateuser.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { UserDetailsService } from '../user-details.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 interface Userdata {
-  id:number;
+  id: number;
   name: string;
   dateOfBirth: Date;
   email: string;
@@ -13,7 +15,6 @@ interface Userdata {
     institutename: string;
     degree: string;
     percentage: number;
-
     hobby?: {
       Reading: boolean;
       Traveling: boolean;
@@ -30,76 +31,108 @@ interface Userdata {
 }
 
 @Component({
-  selector: 'app-show-details',
-  templateUrl: './userdata.component.html',
-  styleUrls: ['./userdata.component.css'],
+  selector: 'app-signup',
+  templateUrl: './updateuser.component.html',
+  styleUrls: ['./updateuser.component.css'],
 })
-export class UserdataComponent implements OnInit {
+export class UpdateuserComponent {
   userDetails: Userdata | undefined;
-  // headers = new Headers({
-  //   'Content-Type': 'application/json',
-  //   Accept: 'application/json',
-  // });
+  date: Date | undefined;
+  userDetailsForm: FormGroup;
+  hobbies: string[] = [
+    'Reading',
+    'Traveling',
+    'Sports',
+    'Music',
+    'Dancing',
+    'Playing',
+    'Coding',
+    'Cooking',
+  ];
+  max: Date = new Date();
+  todayDate = new Date();
+  dataDeleted: boolean = false;
 
-  // httpOptions = {
-  //   headers: this.headers,
-  // };
+  degrees: string[] = ['10th', '12th', 'Bacholars', 'Masters', 'Doctarte'];
+  formGroup: any;
+  password!: number | string;
+  picker!: Date;
 
   constructor(
+    private formbuild: FormBuilder,
     private router: Router,
+    private route: ActivatedRoute,
     private http: HttpClient,
     private userDetailsService: UserDetailsService
-  ) {}
+  ) {
+    this.userDetailsForm = this.formbuild.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
+      dateOfBirth: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+      education: this.formbuild.group({
+        institutename: [''],
+        degree: [''],
+        percentage: ['', [Validators.pattern(/^([0-9]|[1-9][0-9]|100)$/)]],
+        hobby: [''],
+      }),
+      gender: [''],
+      addresses: this.formbuild.array([]),
+    });
+    this.todayDate.setDate(this.todayDate.getDate());
+  }
+
+  get addresses() {
+    return this.userDetailsForm.get('addresses') as FormArray;
+  }
+
+  addAddress() {
+    this.addresses.push(this.formbuild.control(''));
+  }
 
   ngOnInit() {
     this.userDetailsService.userDetailsObservable$.subscribe((userDetails) => {
       if (userDetails) {
-        console.log(userDetails.id)
+        const userId = this.route.snapshot.params['id'];
+        console.log(userId);
         this.http
-          .get<Userdata[]>(' http://localhost:3000/signup')
-          .subscribe((res) => {
-            if (res && res.length > 0) {
-              this.userDetails = res[res.length - 1];
-            } else {
-              alert('Ther is no user exists');
+          .get<Userdata>('http://localhost:3000/signup' + '/' + userId)
+          .subscribe((formData) => {
+            if (formData.address) {
+              for (let a of formData.address) {
+                this.addAddress(); 
+                const lastIndex = this.addresses.length - 1;
+                this.addresses.at(lastIndex).patchValue(a);
+              }
             }
+            
+            this.userDetailsForm.patchValue(formData);
           });
-      } else {
-        this.router.navigate(['/signup']);
       }
     });
   }
 
-  editForm(userDetails: Userdata["id"]) {
-    this.router.navigate(['/updateuser', this.userDetails]);
-
-
-
-  }
-
-  allusersdata() {
-    this.router.navigate(['/alluserdata']);
-  }
-
-  deleteUser(userDetails: Userdata["id"]) {
-    if (this.userDetails) {
-      const userIdToDelete = this.userDetails['id'];
+  updateUser() {
+    if (this.userDetailsForm) {
+      const userId = this.route.snapshot.params['id'];
       this.http
-        .delete(`http://localhost:3000/signup/${userIdToDelete}`)
-        .subscribe(
-          (response) => {
-            console.log(`User with ID ${userIdToDelete} has been deleted.`);
-            if (this.userDetails) {
-              this.userDetails = undefined;
-            }
-          },
-          (error) => {
-            console.error(
-              `Error deleting user with ID ${userIdToDelete}:`,
-              error
-            );
-          }
-        );
+        .put<Userdata>(
+          'http://localhost:3000/signup' + '/' + userId,
+          this.userDetailsForm.value
+        )
+        .subscribe((res) => {
+          console.log('User updated:', res);
+          this.router.navigate(['/userdata']);
+        });
+    } else {
+      console.error(' Cannot update user.');
     }
   }
 }
