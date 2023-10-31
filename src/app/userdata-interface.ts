4777c4a82e31978aae7cfb608dcaad03d3e02c98
Srujan Rajpura <141118639+kevit-srujan-rajpura@users.noreply.github.com>
2023-10-31 18:46:36 +0530

    initial commit

---
+++ src/app/userdata-interface.ts
@@ -0,0 +1,24 @@
+export interface Userdata{
+    id:number;
+    name: string;
+    dateOfBirth: Date;
+    email: string;
+    phone: number;
+    education?: {
+      institutename: string;
+      degree: string;
+      percentage: number;
+      hobby?: {
+        Reading: boolean;
+        Traveling: boolean;
+        Sports: boolean;
+        Music: boolean;
+        Dancing: boolean;
+        Playing: boolean;
+        Coding: boolean;
+        Cooking: boolean;
+      };
+    };
+    gender?: string;
+    address?: { addedAddress: string }[];
+}
